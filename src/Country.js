c043eb5c8771d332ea1e5e721531224e9489d42f
gguilhermelopes <guilhermelopes275@gmail.com>
2022-12-21 00:45:06 -0300

    adjust on div

--- src/Country.js
+++ src/Country.js
@@ -35,56 +35,55 @@ const Country = ({ darkTheme }) => {
             Back
           </button>
         </Link>
-        {
-          <div className={styles.infoWrapper}>
-            <div>
-              <img src={data[0].flags.svg} alt={`${data[0].name}'s flag`} />
-            </div>
-            <div className={styles.divWrapper}>
-              <ul className={styles.listWrapper}>
-                <h1>{data[0].name}</h1>
-                <div>
-                  <li>
-                    <span>Native Name:</span> {data[0].nativeName}
-                  </li>
-                  <li>
-                    <span>Population:</span>{" "}
-                    {data[0].population.toLocaleString("en-US")}
-                  </li>
-                  <li>
-                    <span>Region:</span> {data[0].region}
-                  </li>
-                  <li>
-                    <span>Sub Region:</span> {data[0].subregion}
-                  </li>
-                  <li>
-                    <span>Capital:</span>{" "}
-                    {data[0].capital ? data[0].capital : "None"}
-                  </li>
-                </div>
-                <div>
-                  <li>
-                    <span>Top Level Domain:</span> {data[0].topLevelDomain}
-                  </li>
-                  <li>
-                    <span>Currencies:</span>{" "}
-                    {data[0].currencies ? data[0].currencies[0].name : "None"}
-                  </li>
-                  <li>
-                    <span>Languages:</span>{" "}
-                    {data[0].languages
-                      .map((language) => language.name)
-                      .join(", ")}
-                  </li>
-                </div>
-              </ul>
-              <p>
-                <span>Border Countries: </span>
-                {data[0].borders ? data[0].borders.join(" ") : "None"}
-              </p>
-            </div>
+
+        <div className={styles.infoWrapper}>
+          <div>
+            <img src={data[0].flags.svg} alt={`${data[0].name}'s flag`} />
           </div>
-        }
+          <div className={styles.divWrapper}>
+            <ul className={styles.listWrapper}>
+              <h1>{data[0].name}</h1>
+              <div>
+                <li>
+                  <span>Native Name:</span> {data[0].nativeName}
+                </li>
+                <li>
+                  <span>Population:</span>{" "}
+                  {data[0].population.toLocaleString("en-US")}
+                </li>
+                <li>
+                  <span>Region:</span> {data[0].region}
+                </li>
+                <li>
+                  <span>Sub Region:</span> {data[0].subregion}
+                </li>
+                <li>
+                  <span>Capital:</span>{" "}
+                  {data[0].capital ? data[0].capital : "None"}
+                </li>
+              </div>
+              <div>
+                <li>
+                  <span>Top Level Domain:</span> {data[0].topLevelDomain}
+                </li>
+                <li>
+                  <span>Currencies:</span>{" "}
+                  {data[0].currencies ? data[0].currencies[0].name : "None"}
+                </li>
+                <li>
+                  <span>Languages:</span>{" "}
+                  {data[0].languages
+                    .map((language) => language.name)
+                    .join(", ")}
+                </li>
+              </div>
+            </ul>
+            <p>
+              <span>Border Countries: </span>
+              {data[0].borders ? data[0].borders.join(" ") : "None"}
+            </p>
+          </div>
+        </div>
       </section>
     );
   else return null;
