f1b4102a88194c6a324ac96af4928a72dfd30fd2
gguilhermelopes <guilhermelopes275@gmail.com>
2022-12-23 00:46:15 -0300

    finished project (border countries)

--- src/Country.js
+++ src/Country.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import useFetch from "./Hooks/useFetch";
 import styles from "./Country.module.css";
 import Loading from "./Interface Elements/Loading";
@@ -10,15 +10,16 @@ import { ReactComponent as ArrowDarkMode } from "./Assets/arrow_back_darkmode.sv
 const Country = ({ darkTheme }) => {
   const { name } = useParams();
   const { data, loading, error, request } = useFetch();
+  const navigate = useNavigate();
 
   React.useEffect(() => {
     async function fetchData() {
-      await request(`https://restcountries.com/v2/name/${name}?fullText=true`);
+      await request(`https://restcountries.com/v2/all`);
     }
     fetchData();
   }, [request, name]);
 
-  if (loading) return <Loading />;
+  if (loading) return <Loading darkTheme={darkTheme} />;
   if (error) return <Error />;
   if (data) {
     return (
@@ -29,69 +30,89 @@ const Country = ({ darkTheme }) => {
             : `${styles.countryPage}`
         } `}
       >
-        <Link to="/">
-          <button>
-            {darkTheme ? <ArrowDarkMode /> : <Arrow />}
-            Back
-          </button>
-        </Link>
+        <button onClick={() => navigate(-1)}>
+          {darkTheme ? <ArrowDarkMode /> : <Arrow />}
+          Back
+        </button>
 
-        <div className={styles.infoWrapper}>
-          <div>
-            <img src={data[0].flags.svg} alt={`${data[0].name}'s flag`} />
-          </div>
-          <div className={styles.divWrapper}>
-            <ul className={styles.listWrapper}>
-              <h1>{data[0].name}</h1>
+        {data
+          .filter((value) => {
+            if (name === value.name) {
+              return value;
+            } else return null;
+          })
+          .map((item) => (
+            <div key={item.name} className={styles.infoWrapper}>
               <div>
-                <li>
-                  <span>Native Name:</span> {data[0].nativeName}
-                </li>
-                <li>
-                  <span>Population:</span>{" "}
-                  {data[0].population.toLocaleString("en-US")}
-                </li>
-                <li>
-                  <span>Region:</span> {data[0].region}
-                </li>
-                <li>
-                  <span>Sub Region:</span> {data[0].subregion}
-                </li>
-                <li>
-                  <span>Capital:</span>{" "}
-                  {data[0].capital ? data[0].capital : "None"}
-                </li>
+                <img src={item.flags.svg} alt={`${item.name}'s flag`} />
               </div>
-              <div>
-                <li>
-                  <span>Top Level Domain:</span> {data[0].topLevelDomain}
-                </li>
-                <li>
-                  <span>Currencies:</span>{" "}
-                  {data[0].currencies ? data[0].currencies[0].name : "None"}
-                </li>
-                <li>
-                  <span>Languages:</span>{" "}
-                  {data[0].languages
-                    .map((language) => language.name)
-                    .join(", ")}
-                </li>
-              </div>
-            </ul>
-            <div className={styles.bordersWrapper}>
-              <span>Border Countries</span>
-              <div>
-                <ul className={styles.borders}>
-                  {data[0].borders ? (
-                    data[0].borders.map((item) => <li key={item}>{item}</li>)
-                  ) : (
-                    <li>None</li>
-                  )}
+              <div className={styles.divWrapper}>
+                <ul className={styles.listWrapper}>
+                  <h1>{item.name}</h1>
+                  <div>
+                    <li>
+                      <span>Native Name:</span> {item.nativeName}
+                    </li>
+                    <li>
+                      <span>Population:</span>{" "}
+                      {item.population.toLocaleString("en-US")}
+                    </li>
+                    <li>
+                      <span>Region:</span> {item.region}
+                    </li>
+                    <li>
+                      <span>Sub Region:</span> {item.subregion}
+                    </li>
+                    <li>
+                      <span>Capital:</span>{" "}
+                      {item.capital ? item.capital : "None"}
+                    </li>
+                  </div>
+                  <div>
+                    <li>
+                      <span>Top Level Domain:</span> {item.topLevelDomain}
+                    </li>
+                    <li>
+                      <span>Currencies:</span>{" "}
+                      {item.currencies ? item.currencies[0].name : "None"}
+                    </li>
+                    <li>
+                      <span>Languages:</span>{" "}
+                      {item.languages
+                        .map((language) => language.name)
+                        .join(", ")}
+                    </li>
+                  </div>
                 </ul>
+                <div className={styles.bordersWrapper}>
+                  <span>Border Countries</span>
+                  <div>
+                    <ul
+                      className={`${
+                        darkTheme
+                          ? `darkTheme ${styles.borders}`
+                          : `${styles.borders}`
+                      }`}
+                    >
+                      {item.borders ? (
+                        data
+                          .filter((value) => {
+                            return item.borders.includes(value.alpha3Code);
+                          })
+                          .map((value) => (
+                            <Link to={`/country/${value.name}`}>
+                              <li key={value.name}>{value.name}</li>
+                            </Link>
+                          ))
+                      ) : (
+                        <li>None</li>
+                      )}
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+          ))}
       </section>
     );
   } else return null;
--- src/Interface Elements/Error.js
+++ src/Interface Elements/Error.js
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./Error.module.css";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className={`container ${styles.error}`}>
+      An error occurred. Please return to the Homepage or try again later.
+    </div>
+  );
 };
 
 export default Error;
